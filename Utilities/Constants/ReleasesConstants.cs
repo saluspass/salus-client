843cb5a19064a52614f416b260aef9b1c4826893
viperman1271 <viperman1271@hotmail.com>
2017-11-09 18:32:43 -0500

    New Release Constants file

---
+++ Utilities/Constants/ReleasesConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace ipfs_pswmgr
+{
+    public static class ReleasesConstants
+    {
+        public static readonly string ApiEndpoint = "https://api.github.com/repos/viperman1271/ipfs-pswmgr/releases";
+    }
+}
