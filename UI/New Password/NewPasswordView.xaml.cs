281662519acb75e9a6cb2dd7796b8ca8d9561726
viperman1271 <viperman1271@hotmail.com>
2017-11-09 14:36:50 -0500

    Added new password view and view model

---
+++ UI/New Password/NewPasswordView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace ipfs_pswmgr
+{
+    /// <summary>
+    /// Interaction logic for NewPasswordView.xaml
+    /// </summary>
+    public partial class NewPasswordView : Window
+    {
+        #region Variables
+
+        private readonly NewPasswordViewModel _ViewModel;
+
+        #endregion
+
+        #region Ctor
+
+        public NewPasswordView(PasswordEntry entry = null)
+        {
+            _ViewModel = new NewPasswordViewModel(this, entry);
+            DataContext = _ViewModel;
+
+            InitializeComponent();
+
+            _PasswordBox.Password = _ViewModel.Model.Password;
+        }
+
+        #endregion
+
+        #region Properties
+
+        internal NewPasswordViewModel ViewModel
+        {
+            get { return _ViewModel; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void PasswordBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _ViewModel.OnPasswordBoxPasswordChanged();
+        }
+
+        private void LoginView_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            _ViewModel.OnPreviewKeyUp(e.Key);
+        }
+
+        #endregion
+    }
+}
---
+++ UI/New Password/NewPasswordViewModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ipfs_pswmgr
+{
+    class NewPasswordViewModel : INotifyPropertyChanged
+    {
+        #region Variables
+
+        private readonly NewPasswordView _View;
+        private readonly PasswordEntry _Model;
+        private readonly DelegateCommand _OKCommand;
+        private readonly bool _ExistingEntry;
+
+        #endregion
+
+        #region Ctor
+
+        public NewPasswordViewModel(NewPasswordView view, PasswordEntry entry = null)
+        {
+            _ExistingEntry = entry != null;
+            _View = view;
+            _Model = entry ?? new PasswordEntry();
+            _OKCommand = new DelegateCommand(OnOk, DataCompleted);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public PasswordEntry Model
+        {
+            get { return _Model; }
+        }
+
+        public bool DataCompleted(object obj)
+        {
+            return !string.IsNullOrEmpty(_Model.Name) && !string.IsNullOrEmpty(_Model.Website) && !string.IsNullOrEmpty(_Model.Password) && !string.IsNullOrEmpty(_Model.Username);
+        }
+
+        public ICommand OKCommand
+        {
+            get { return _OKCommand; }
+        }
+
+        internal void OnPasswordBoxPasswordChanged()
+        {
+            _Model.Password = _View._PasswordBox.Password;
+            _OKCommand.RaiseCanExecuteChanged();
+        }
+
+        public string Name
+        {
+            get { return _Model.Name; }
+            set
+            {
+                if (_Model.Name != value)
+                {
+                    _Model.Name = value;
+                    OnPropertyChanged();
+                    _OKCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public string Username
+        {
+            get { return _Model.Username; }
+            set
+            {
+                if (_Model.Username != value)
+                {
+                    _Model.Username = value;
+                    OnPropertyChanged();
+                    _OKCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public string Website
+        {
+            get { return _Model.Website; }
+            set
+            {
+                if (_Model.Website != value)
+                {
+                    _Model.Website = value;
+                    OnPropertyChanged();
+                    _OKCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public string WindowTitle
+        {
+            get { return _ExistingEntry ? "Modify Password" : "Add Password"; }
+        }
+
+        public string ButtonContent
+        {
+            get { return _ExistingEntry ? "Modify" : "Add"; }
+        }
+
+        public bool AccountNameEnabled
+        {
+            get { return !_ExistingEntry; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OnOk()
+        {
+            _View.DialogResult = true;
+            _View.Close();
+        }
+
+        internal void OnPreviewKeyUp(Key key)
+        {
+            if ((key == Key.Enter || key == Key.Return) && _OKCommand.CanExecute(null))
+            {
+                _OKCommand.Execute(null);
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
