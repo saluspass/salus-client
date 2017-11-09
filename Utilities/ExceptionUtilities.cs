43a041ba957fb181d4a3f2688b233d9c98c8e871
viperman1271 <viperman1271@hotmail.com>
2017-11-09 14:20:25 -0500

    Added Exception Utilities

---
+++ Utilities/ExceptionUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ipfs_pswmgr
+{
+    class ExceptionUtilities
+    {
+        public static void TryCatchIgnore(Action a)
+        {
+            try
+            {
+                a();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public static T TryAssignCatchIgnore<T>(Func<T> a, T defaultReturn)
+        {
+            T returnValue = defaultReturn;
+            try
+            {
+                returnValue = a();
+            }
+            catch
+            {
+
+            }
+            return returnValue;
+        }
+    }
+}
