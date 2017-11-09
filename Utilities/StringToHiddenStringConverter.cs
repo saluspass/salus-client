85090fb0e76e0a6e6a5386bd24887e8666fada6a
viperman1271 <viperman1271@hotmail.com>
2017-11-09 14:20:16 -0500

    Added StringToHiddentStringConverter

---
+++ Utilities/StringToHiddenStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace ipfs_pswmgr
+{
+    class StringToHiddenStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(string))
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (char c in (value as string))
+                {
+                    builder.Append("*");
+                }
+                return builder.ToString();
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
