e7af3f675b9c00a9262e3778a42bcf995388798a
viperman1271 <viperman1271@hotmail.com>
2017-11-09 14:36:42 -0500

    Added Options View and View Model

---
+++ UI/OptionsView/OptionsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace ipfs_pswmgr
+{
+    /// <summary>
+    /// Interaction logic for OptionsView.xaml
+    /// </summary>
+    public partial class OptionsView : Window
+    {
+        #region Variable
+
+        private readonly OptionsViewModel _ViewModel;
+
+        #endregion
+
+        #region Ctor
+
+        public OptionsView()
+        {
+            _ViewModel = new OptionsViewModel(this);
+            DataContext = _ViewModel;
+
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ UI/OptionsView/OptionsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace ipfs_pswmgr
+{
+    class OptionsViewModel
+    {
+        private readonly OptionsView _View;
+
+        #region Ctor
+
+        public OptionsViewModel(OptionsView view)
+        {
+            _View = view;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Conf Model
+        {
+            get { return App.Instance.Conf; }
+        }
+
+        public ICommand OKCommand
+        {
+            get { return new DelegateCommand(delegate { Model.Save("pswmgr.conf"); _View.DialogResult = true; _View.Close(); }); }
+        }
+
+        public ICommand CancelCommand
+        {
+            get { return new DelegateCommand(delegate { _View.DialogResult = false; _View.Close(); }); }
+        }
+
+        #endregion
+    }
+}
