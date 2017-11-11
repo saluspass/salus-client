7fe3176798c08dba38608cb3546ea19f67d3d6bb
viperman1271 <viperman1271@hotmail.com>
2017-11-10 21:03:02 -0500

    More rebranding

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ipfs-pswmgr")]
+[assembly: AssemblyTitle("Salus Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ipfs-pswmgr")]
+[assembly: AssemblyProduct("Salus Client")]
 [assembly: AssemblyCopyright("Copyright © Mike Filion 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
