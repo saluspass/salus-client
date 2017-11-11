093c8f218ad85bb21585c7f9b592861c2e6ed3ef
viperman1271 <viperman1271@hotmail.com>
2017-11-11 15:32:15 -0500

    Namespace change

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SalusClient.Properties {
+namespace Salus.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
