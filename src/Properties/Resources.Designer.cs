2ec06022f4bcdeda89ecd8f5cea2f0492cea4d19
viperman1271 <viperman1271@hotmail.com>
2017-11-11 15:30:10 -0500

    Default namespace change

--- src/Properties/Resources.Designer.cs
+++ src/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SalusClient.Properties {
+namespace Salus.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SalusClient.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SalusClient.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Salus.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
